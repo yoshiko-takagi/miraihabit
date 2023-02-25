6b24c63b1257887e422698654d78baef8939f40a
Emily O'Marra <emilyomarra@hotmail.com>
2023-02-25 12:55:42 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "/dashboard", to: "users#dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # the user can see their challenges on their challenges page
+  resources :challenges, only: [:index, :show ]
+  get "/calendar", to: "user_challenges#calendar"
+  resources :user_challenges, only: [:update]
+  # the assessment can be updated for the results
+  resources :user_answers, only: [:index]
+  # the questions in the assessment can be seen and updated for results
+  namespace :questions, only: [:show] do
+    resources :user_answers, only: [:create]
+  end
+  resources :challenges, only: [:index] do
+    resources :user_challenges, only: [:create]
+  end
 end
