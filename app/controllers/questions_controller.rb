7ffda301d0328ac9d823883a39310c43f3599114
Miriam Fiandanese <mmiy55@gmail.com>
2023-02-28 17:31:30 +0900

    add scope to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,12 @@
 class QuestionsController < ApplicationController
   def show
-    @question = Question.find(params[:id])
+    @question = all_questions.find(params[:id])
     authorize @question
   end
+
+  private
+  def all_questions
+    policy_scope(Question)
+  end
+
 end
