63042a8f191670ddb9938a01679c71dc5894d90b
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-01 15:56:23 +0900

    add association user-answer to question and conditional on redirection

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,8 @@
 class QuestionsController < ApplicationController
   def show
     @question = all_questions.find(params[:id])
-    @next_question = all_questions.where("id > ?", @question.id).first
-    @previous_question = all_questions.where("id < ?", @question.id).last
+    # @next_question = all_questions.where("id > ?", @question.id).first
+    # @previous_question = all_questions.where("id < ?", @question.id).last
     authorize @question
     @user_answer = UserAnswer.new
   end
--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -6,11 +6,21 @@ class UserAnswersController < ApplicationController
     @user_answer = UserAnswer.new(user_answer_params)
     @user_answer.user = current_user
     authorize @user_answer
-    if !@user_answer.save
+    if @user_answer.save
+      # give the user some feedback
+      sleep(1)
+      if @user_answer.question != Question.last
+        # move to next question
+        @next_question = Question.where("id > ?", @user_answer.answer.question.id).first
+        redirect_to question_path(@next_question)
+      else
+        console.log("fine")
+        # submit > go to results
+      end
+    else
       render 'questions/show', status: :unprocessable_entity
     end
   end
-
 
   private
 
