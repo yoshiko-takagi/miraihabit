dea040774d2bef0d4801fd2a0d42b1545377daca
Chieko Sato <chiekosato.02@gmail.com>
2023-03-01 11:06:39 +0900

    finished pundit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # the user can see their challenges on their challenges page
   resources :challenges, only: [:index, :show ]
-  get "/calendar", to: "user_challenges#calendar"
+  get "/calendar", to: "user_challenges#index"
   resources :user_challenges, only: [:update]
   # the assessment can be updated for the results
   resources :user_answers, only: [:index]
