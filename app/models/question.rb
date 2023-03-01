2ab1803738dd449f426dee3fd9ae0f03e7a06012
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-01 17:49:25 +0900

    set dependent destroy on answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  has_many :user_answers
-  has_many :answers
+  # has_many :user_answers
+  has_many :answers, dependent: :destroy
   validates :content, presence: true
 end
