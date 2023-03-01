afe79abd4c768505ba11ce582383377b5ae663d8
Yoshiko Takagi <yoshiko.takagi49@gmail.com>
2023-03-01 13:57:01 +0900

    final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,40 @@
 class UsersController < ApplicationController
 
   def dashboard
-    if user_answer_filtered
-    @currect_score = user_score + completed_challenge_score
-    end
-    authorize currect_user
-  end
-
-  private
-
-  def user_score
-    @user = User.find(params[:id])
+    @user = current_user
     @user_score = @user.score
-    # @challenge_score = UserChallenge.find(params[:challenge_id])
-
+    authorize current_user
   end
 
-  # if user answers option 1 during assessment test, its question_id shouldn't be included in challenges Model.
-  def user_answer_filtered
-    @user_answer = UserAnswer.find(params[:answer_id])
-    @answer = Answer.content.last # option 2
-    if @user_answer.answer_id == @answer
-      @user_answer.save
-    end
-  end
 
-  # if user's challenge_id is completed, save the score of that challenge.
-  #
-  def completed_challenge_score
+  # def dashboard
+  #   if user_answer_filtered
+  #   @currect_score = user_score + completed_challenge_score
+  #   end
+  #   authorize currect_user
+  # end
 
-  end
+  # private
+
+  # def user_score
+  #   @user = User.find(params[:id])
+  #   @user_score = @user.score
+  #   # @challenge_score = UserChallenge.find(params[:challenge_id])
+
+  # end
+
+  # # if user answers option 1 during assessment test, its question_id shouldn't be included in challenges Model.
+  # def user_answer_filtered
+  #   @user_answer = UserAnswer.find(params[:answer_id])
+  #   @answer = Answer.content.last # option 2
+  #   if @user_answer.answer_id == @answer
+  #     @user_answer.save
+  #   end
+  # end
+
+  # # if user's challenge_id is completed, save the score of that challenge.
+  # #
+  # def completed_challenge_score
+
+  # end
 end
