a78767eacebb53d56faf467075aa2235b7868e1e
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-02 12:13:52 +0900

    set dependency to run the seeds correctly

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  has_one :challenge
-
+  has_one :challenge, dependent: :destroy
+  has_many :user_answers, dependent: :destroy
   validates :content, presence: true
 end
