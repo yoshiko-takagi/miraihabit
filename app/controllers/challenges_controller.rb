8f81f04ea23b916db4827192d96227e10930d439
Emily O'Marra <emilyomarra@hotmail.com>
2023-03-03 15:45:27 +0900

    fix info button, add bottom navbar

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,17 +1,15 @@
 class ChallengesController < ApplicationController
 
   def index
-    if params[:challenge].present?
-      if params[:challenge][:category].present? && params[:challenge][:difficulty].present?
-        @challenges = policy_scope(Challenge).where(category: params[:challenge][:category].downcase, difficulty: params[:challenge][:difficulty].to_i)
-      elsif params[:challenge][:category].present?
-        @challenges = policy_scope(Challenge).where(category: params[:challenge][:category].downcase)
-      elsif params[:challenge][:difficulty].present?
-         @challenges = policy_scope(Challenge).where(difficulty: params[:challenge][:difficulty].to_i)
-      end
-    else
-    @challenges = policy_scope(Challenge)
-    end
+      if params.dig(:challenge, :category).present? && params.dig(:challenge, :difficulty).present?
+        @challenges = policy_scope(Challenge).where(category: params.dig(:challenge, :category).downcase, difficulty: params.dig(:challenge, :difficulty).to_i)
+      elsif params.dig(:challenge, :category).present?
+        @challenges = policy_scope(Challenge).where(category: params.dig(:challenge, :category).downcase)
+      elsif params.dig(:challenge, :difficulty).present?
+         @challenges = policy_scope(Challenge).where(difficulty: params.dig(:challenge, :difficulty).to_i)
+        else
+          @challenges = policy_scope(Challenge)
+        end
     @user_challenge = UserChallenge.new
   end
 
