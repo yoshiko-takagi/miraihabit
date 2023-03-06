7bdea6668e9e9bd7bc9b47e5226d97b01a4ac053
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-06 16:06:50 +0900

    add tracker route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # the user can see their challenges on their challenges page
   resources :challenges, only: [:index, :show ]
   get "/calendar", to: "user_challenges#index"
+  # add tracker page
+  get "/tracker", to: "user_challenges#tracker"
 
   resources :user_challenges, only: [:update]
   resources :user_answers, only: [:index]
