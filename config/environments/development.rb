ed82e1a6bf68e507646fe7de6fee1a5eb2a4f516
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-07 09:11:20 +0900

    put host config clear line in the end

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts.clear
+
   config.hosts << /[a-z0-9\-]+\.loca\.lt/
+  config.hosts.clear
 end
