fd6b3e3d8be15f203e24604ea287c892751a1ce0
Chieko Sato <chiekosato.02@gmail.com>
2023-03-07 11:51:12 +0900

    added photos

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,6 +2,7 @@ class Challenge < ApplicationRecord
   belongs_to :answer, optional: true
   has_many :user_challenges, dependent: :destroy
   has_many :users, through: :user_challenges
+  has_one_attached :photo
 
   validates :difficulty, :name, :description, :max_score, :duration, presence: true
 end
