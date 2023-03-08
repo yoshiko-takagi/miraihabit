e8b2378116cf2a65626f0ee93731e537103e8c38
Miriam Fiandanese <mmiy55@gmail.com>
2023-03-08 12:51:24 +0900

    update user route for merit use with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   root to: "pages#home"
   # get "/dashboard", to: "users#dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
